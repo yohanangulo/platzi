9179cb1954a40157e3bc600b3d05662ecfa9dd7b
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-01-05 19:06:56 -0400

    update

--- base-de-datos-con-posgresql/db/migrations/20231221155817-create-user.js
+++ base-de-datos-con-posgresql/db/migrations/20231221155817-create-user.js
@@ -1,10 +1,36 @@
 'use strict'
-const { UserSchema, USER_TABLE } = require('../models/user.model')
+const { DataTypes } = require('sequelize')
+const { USER_TABLE } = require('../models/user.model')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserSchema)
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: DataTypes.NOW,
+      },
+    })
   },
 
   async down(queryInterface) {
