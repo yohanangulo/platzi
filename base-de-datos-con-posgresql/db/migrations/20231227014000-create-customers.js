dcf83610bb84e7733b9e1114fc9df08f69f75a92
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-01-05 19:17:25 -0400

    update

--- base-de-datos-con-posgresql/db/migrations/20231227014000-create-customers.js
+++ base-de-datos-con-posgresql/db/migrations/20231227014000-create-customers.js
@@ -1,10 +1,53 @@
 'use strict'
-const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customer.model')
+const { DataTypes } = require('sequelize')
+const { CUSTOMER_TABLE } = require('../models/customer.model')
+const { USER_TABLE } = require('../models/user.model')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema)
+    await queryInterface.createTable(CUSTOMER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name',
+      },
+
+      phone: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: DataTypes.NOW,
+      },
+
+      userId: {
+        field: 'user_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: USER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+    })
   },
 
   async down(queryInterface) {
