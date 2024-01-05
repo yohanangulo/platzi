a77f1c426b726d3636968333580f53a5b7c39275
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-01-05 19:58:24 -0400

    update

--- base-de-datos-con-posgresql/db/migrations/20231228142400-create-order.js
+++ base-de-datos-con-posgresql/db/migrations/20231228142400-create-order.js
@@ -1,11 +1,39 @@
 'use strict'
 
-const { ORDER_TABLE, OrderSchema } = require('../models/order.model')
+const { DataTypes } = require('sequelize')
+const { ORDER_TABLE } = require('../models/order.model')
+const { CUSTOMER_TABLE } = require('../models/customer.model')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema)
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+
+      customerId: {
+        field: 'customer_id',
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: DataTypes.NOW,
+      },
+    })
   },
 
   async down(queryInterface) {
