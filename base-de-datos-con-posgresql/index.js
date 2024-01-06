656b6ed4d5744d1cee5833604dbd7b5b3fd1914b
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-01-06 13:32:21 -0400

    final version

--- base-de-datos-con-posgresql/index.js
+++ base-de-datos-con-posgresql/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const routerApi = require('./routes')
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler')
+const config = require('./config/config')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,8 +13,10 @@ app.use(express.json())
 const whitelist = ['http://localhost:8080', 'https://myapp.co']
 const options = {
   origin: (origin, callback) => {
-    // detele !origin when it's production
-    if (whitelist.includes(origin) || !origin) {
+    // detele !origin when it's production or use the condition down below
+
+    if (whitelist.includes(origin) || (!config.isProd && !origin)) {
+      // if (whitelist.includes(origin) || !origin) {
       callback(null, true)
     } else {
       callback(new Error('no permitido'))
