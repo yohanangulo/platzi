54931228790983d11bb49ebe78473aa09445a8ef
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2024-01-23 08:20:01 -0400

    update

--- curso-nodejs-auth/services/category.service.js
+++ curso-nodejs-auth/services/category.service.js
@@ -1,39 +1,45 @@
-const boom = require('@hapi/boom');
+const boom = require('@hapi/boom')
 
-const { models }= require('./../libs/sequelize');
+const { models } = require('./../libs/sequelize')
 
 class CategoryService {
-
-  constructor(){
-  }
+  constructor() {}
   async create(data) {
-    const newCategory = await models.Category.create(data);
-    return newCategory;
+    const newCategory = await models.Category.create(data)
+    return newCategory
   }
 
   async find() {
-    const categories = await models.Category.findAll();
-    return categories;
+    const categories = await models.Category.findAll()
+    return categories
   }
 
   async findOne(id) {
     const category = await models.Category.findByPk(id, {
-      include: ['products']
-    });
-    return category;
+      include: ['products'],
+    })
+
+    if (!category) {
+      throw boom.notFound('category not found')
+    }
+
+    return category
   }
 
   async update(id, changes) {
     return {
       id,
       changes,
-    };
+    }
   }
 
   async delete(id) {
-    return { id };
-  }
+    const category = await this.findOne(id)
 
+    category.destroy()
+
+    return { id }
+  }
 }
 
-module.exports = CategoryService;
+module.exports = CategoryService
